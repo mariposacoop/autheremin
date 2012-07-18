3285f1f259864f7e44276ed32fb31b8a0079d3cb
Gabriel Farrell <g@grrawr.com>
2012-07-18 17:12:55 -0400

    Added test-verify

---
+++ test/test-verify.js
@@ -0,0 +1,27 @@
+var assert = require('assert')
+var autheremin = require('../autheremin')
+var bcrypt = require('bcrypt')
+
+var username = 'joan'
+var password = 'non34Aro'
+
+// stub out db
+var db = function() {}
+db.store = {}
+db.store['autheremin:'+username] = bcrypt.hashSync(password, 8)
+db.get = function(key, cb) {
+  cb(null, db.store[key])
+}
+
+var autheremin = require('../autheremin')(db)
+
+var verified = false
+
+autheremin.verify(username, password, function(err) {
+  if (err) throw err
+  verified = true
+})
+
+process.on('exit', function() {
+  assert(verified)
+})
