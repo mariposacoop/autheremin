d938de9275bd61949662f3f0c9201db5a9680e10
Gabriel Farrell <g@grrawr.com>
2012-07-18 17:05:58 -0400

    Added create method

--- autheremin.js
+++ autheremin.js
@@ -2,14 +2,28 @@ var bcrypt = require('bcrypt')
 
 module.exports = function(db) {
   var autheremin = {}
+
   autheremin.create = function(username, password, cb) {
     bcrypt.hash(password, 8, function(err, hash) {
       if (err) return cb(err)
-      db.set(username, hash, function(err) {
+      db.set('autheremin:'+username, hash, function(err) {
         if (err) return cb(err)
         cb()
       })
     })
   }
+  
+  autheremin.verify = function(username, password, cb) {
+    db.get('autheremin:'+username, function(err, hash) {
+      if (err) return cb(err)
+      if (!hash) return cb(new Error('No match'))
+      bcrypt.compare(password, hash, function(err, res) {
+        if (err) return cb(err)
+        if (!res) return cb(new Error('No match'))
+        cb()
+      })
+    })
+  }
+
   return autheremin
 }
